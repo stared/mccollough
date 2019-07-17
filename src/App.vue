edbf5816d0f2d5681d4ce69337940a0e44d6a834
Piotr Migdał <pmigdal@gmail.com>
2019-07-17 16:44:56 +0200

    mccollough init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PatternPlace
+      :background='background'
+      :pattern='pattern'
+      :width='800'
+      :height='800'
+    />
+    <div>
+      Background:
+      <button @click='setBackground("white")'>White</button>
+      <button @click='setBackground("#f00")'>Red</button>
+      <button @click='setBackground("#0f0")'>Green</button>
+    </div>
+    <div>
+      Pattern:
+      <button @click='setPattern("CirclePattern")'>Circles</button>
+      <button @click='setPattern("CrossesPattern")'>Crosses</button>
+    </div>
+    <div>
+      Adapt:
+      <button @click='setPattern("CirclePattern"); setBackground("#f00")'>Stage 1</button>
+      <button @click='setPattern("CrossesPattern"); setBackground("#0f0")'>Stage 2</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PatternPlace from './components/PatternPlace.vue'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      background: 'red',
+      pattern: 'CirclePattern'
+    };
+  },
   components: {
-    HelloWorld
+    PatternPlace
+  },
+  methods: {
+    'setBackground': function(color) {
+      this.background = color;
+    },
+    'setPattern': function(patternName) {
+      this.pattern = patternName;
+    },
   }
+
 }
 </script>
 
@@ -24,5 +59,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
---
+++ src/components/PatternPlace.vue
@@ -0,0 +1,42 @@
+<template>
+  <svg class="patternplace"
+    :width='width' :height='height'
+    xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <pattern id="CirclePattern" x="0" y="0" patternUnits="userSpaceOnUse" width="50" height="50">
+        <circle cx="25" cy="25" r="15" fill-opacity="0" stroke="black" stroke-width="5px"/>
+      </pattern>
+      <pattern id="CrossesPattern" x="0" y="0" patternUnits="userSpaceOnUse" width="50" height="50">
+        <line x1="0" y1="0" x2="50" y2="50"
+        stroke="black" stroke-width="5px"/>
+        <line x1="0" y1="50" x2="50" y2="0"
+        stroke="black" stroke-width="5px"/>
+      </pattern>
+    </defs>
+
+    <rect :fill='background' :width='width' :height='height'/>
+    <rect :fill='`url(#${pattern})`' stroke="black" :width='width' :height='height'/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'PatternPlace',
+  props: {
+    background: String,
+    pattern: String,
+    width: {
+      type: Number,
+      default: 600
+    },
+    height: {
+      type: Number,
+      default: 600
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
